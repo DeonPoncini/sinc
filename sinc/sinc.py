4013f93d76e8a3c3c0e621873771a22e51fab8c9
Deon Poncini <dex1337@gmail.com>
2015-02-02 22:51:46 -0800

    Update reader to conform to schema
    We now read XML formats in the schema definition, and validate them
    first

--- sinc/sinc.py
+++ sinc/sinc.py
@@ -7,7 +7,7 @@
 
 import sys
 
-from xml.dom import minidom
+from lxml import etree
 
 from sinc_ast import Package, Enumeration, Constant, Structure, Ast
 from sinc_c import CVisitor
@@ -15,62 +15,61 @@ from sinc_cpp import CppVisitor
 from sinc_java import JavaVisitor
 from sinc_python import PythonVisitor
 
-if len(sys.argv) < 3:
-    print "Usage is: " + sys.argv[0] + " <data.xml> <outpath> "
+if len(sys.argv) < 4:
+    print "Usage is: " + sys.argv[0] + " <xml> <outpath> <schema>"
     sys.exit(1)
 
-dataDom = minidom.parse(sys.argv[1])
+doc = etree.parse(sys.argv[1])
 outPath = sys.argv[2]
+xmlschema_doc = etree.parse(sys.argv[3])
+xmlschema = etree.XMLSchema(xmlschema_doc)
 
-# get the output file name
-datas = dataDom.getElementsByTagName('data')
-if len(datas) > 1:
-    print "Only one data element allowed"
+if not xmlschema.validate(doc):
+    xmlschema.assertValid(doc)
     sys.exit(1)
 
-fileName = datas[0].getAttribute('name')
+root = doc.getroot()
+fileName = root.attrib['name']
 
 # get the package declaration
-packages = dataDom.getElementsByTagName('package')
+packages = doc.findall('package')
 
-if len(packages) > 1:
-    print "Only one package element allowed"
-    sys.exit(1)
-
-uri = packages[0].getAttribute('uri')
-namespace = packages[0].getElementsByTagName('namespace')
+uri = packages[0].findall('uri')[0].text
+namespace = packages[0].findall('namespaces')[0].findall('namespace')
 package = Package(uri)
 for n in namespace:
-    package.add_namespace(n.childNodes[0].nodeValue)
+    package.add_namespace(n.text)
 ast = Ast(package)
 
 # parse all the enums
-enums = dataDom.getElementsByTagName('enum')
+enums = root.findall('enum')
 for e in enums:
-    name = e.getAttribute('name')
-    entries = e.getElementsByTagName('entry')
+    name = e.findall('name')[0].text
+    entries = e.findall('entries')[0]
+    entry = entries.findall('entry')
     enumeration = Enumeration(name)
-    for e1 in entries:
-        enumeration.add_entry(e1.childNodes[0].nodeValue)
+    for e1 in entry:
+        enumeration.add_entry(e1.text)
     ast.add_enum(enumeration)
 
 # parse all the constants
-constants = dataDom.getElementsByTagName('constant')
+constants = root.findall('assignment')
 for c in constants:
-    name = c.getAttribute('name')
-    dataType = c.getAttribute('type')
-    value = c.childNodes[0].nodeValue
+    name = c.findall('name')[0].text
+    dataType = c.findall('typedecl')[0].findall('type')[0].findall('base')[0].text
+    value = c.findall('value')[0].text
     ast.add_constant(Constant(name, dataType, value))
 
 # parse all the structs
-structs = dataDom.getElementsByTagName('struct')
+structs = root.findall('struct')
 for s in structs:
-    name = s.getAttribute('name')
-    elements = s.getElementsByTagName('element')
+    name = s.findall('name')[0].text
+    declarations = s.findall('declarations')[0]
+    declaration = declarations.findall('declaration')
     structure = Structure(name)
-    for e in elements:
-        name = e.childNodes[0].nodeValue
-        dataType = e.getAttribute('type')
+    for d in declaration:
+        name = d.findall('name')[0].text
+        dataType = d.findall('typedecl')[0].findall('type')[0].findall('base')[0].text
         structure.add_element(Constant(name, dataType, 0))
     ast.add_structure(structure)
 
